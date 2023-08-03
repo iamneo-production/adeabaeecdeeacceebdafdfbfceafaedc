03777ca5ec959b6ab5b4991779d7ee95dd7f26fa
Iamneo <techadmin@iamneo.ai>
2023-08-03 15:54:38 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/ServiceClass.java
+++ springapp/src/main/java/com/examly/springapp/ServiceClass.java
@@ -1,5 +1,23 @@
 package com.examly.springapp;
 
+import java.util.List;
+import java.util.Optional;
+
 public class ServiceClass {
+
+    public List<ModelTask> findAllTask() {
+        return null;
+    }
+
+    public ModelTask addTask(ModelTask mt) {
+        return null;
+    }
+
+    public void deleteTask(Integer valueOf) {
+    }
+
+    public Optional<ModelTask> getTaskById(Integer valueOf) {
+        return null;
+    }
     
 }

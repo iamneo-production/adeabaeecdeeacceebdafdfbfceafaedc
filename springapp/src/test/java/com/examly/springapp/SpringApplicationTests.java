87bd08c54178da6d1ecb7f386dfd326b89a6069e
Iamneo <techadmin@iamneo.ai>
2023-08-04 11:49:34 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -3,7 +3,7 @@ package com.examly.springapp;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test; 
+import org.junit.*; 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
